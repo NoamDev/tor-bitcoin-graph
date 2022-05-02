3287b9032da3df6cf5faa8a358d53ca1c083f455
NoamDev <noam.yem@gmail.com>
2022-05-02 17:11:11 +0300

    fix wallet's tx balance format

--- webapp/frontend/src/components/WalletDetails/WalletDetails.tsx
+++ webapp/frontend/src/components/WalletDetails/WalletDetails.tsx
@@ -32,7 +32,7 @@ export default function WalletDetails(props: WalletDetailsProps) {
                 <Grid item>
                     {wallet.num_out_txs}
                 </Grid>
-                {wallet.last_in_tx_hash ?
+                {wallet.last_in_tx_hash &&
                     [(<Grid item>
                         <b>Last Incoming Transaction</b>
                     </Grid>),
@@ -43,16 +43,16 @@ export default function WalletDetails(props: WalletDetailsProps) {
                         <b>Last Incoming Transaction Amount</b>
                     </Grid>),
                     (<Grid item>
-                        {wallet.last_in_tx_amount}
+                        {formatBalance(wallet.last_in_tx_amount!)} BTC
                     </Grid>),
                     (<Grid item>
                         <b>Last Incoming Transaction Date</b>
                     </Grid>),
                     (<Grid item>
                         {formatDate(wallet.last_in_tx_date!)}
-                    </Grid>)] : []
+                    </Grid>)]
                 }
-                {wallet.last_out_tx_hash ?
+                {wallet.last_out_tx_hash &&
                     [(<Grid item>
                         <b>Last Outgoing Transaction</b>
                     </Grid>),
@@ -63,14 +63,14 @@ export default function WalletDetails(props: WalletDetailsProps) {
                         <b>Last Outgoing Transaction Amount</b>
                     </Grid>),
                     (<Grid item>
-                        {wallet.last_out_tx_amount}
+                        {formatBalance(wallet.last_out_tx_amount!)} BTC
                     </Grid>),
                     (<Grid item>
                         <b>Last Outgoing Transaction Date</b>
                     </Grid>),
                     (<Grid item>
                         {formatDate(wallet.last_out_tx_date!)}
-                    </Grid>)] : []
+                    </Grid>)]
                 }
                 {wallet.first_in_tx_date ?
                     [(<Grid item>
